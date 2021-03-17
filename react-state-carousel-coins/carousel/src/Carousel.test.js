1f3aba570dd7303ba0d2f1ee34369f09c9d406d8
David <david@Davids-MBP.fios-router.home>
2021-03-17 00:24:40 -0400

    Bug Exausting the image array

--- react-state-carousel-coins/carousel/src/Carousel.test.js
+++ react-state-carousel-coins/carousel/src/Carousel.test.js
@@ -29,9 +29,6 @@ it("works when you click on the right arrow", function() {
   expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
 });
 
-// Write a test that expects that when I am in the second image(Photo by Pratik Patel on Unplash)
-// Clicking left arrow (<-) will move to the first image(photo by Richard Pasquarella on unsplah)
-
 
 it("Should move to the first image when I click the left arrow when I am in the second image", function (){
   const {queryByTestId, queryByAltText} = render(<Carousel />)
@@ -55,4 +52,27 @@ it("Should move to the first image when I click the left arrow when I am in the 
   //Expect firts Image to show
   expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument()
   expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument()
+})
+
+//Left arrow is missing when I am in the first image 
+// Right arrow is missing when I am in the last image
+
+it("should not be left arrow in the first image and It should be a right arrow in the last image", function (){
+  const {queryByTestId} = render(<Carousel />)
+  const leftArrow = queryByTestId("left-arrow")
+  const rightArrow = queryByTestId("right-arrow")
+
+  // In the first image Without left arrow
+  expect(leftArrow).toHaveClass("hidden")
+  expect(rightArrow).not.toHaveClass("hidden")
+
+  fireEvent.click(rightArrow)
+
+  expect(leftArrow).not.toHaveClass("hidden")
+  expect(rightArrow).not.toHaveClass("hidden")
+
+  fireEvent.click(rightArrow)
+
+  expect(leftArrow).not.toHaveClass("hidden")
+  expect(rightArrow).toHaveClass("hidden")
 })
